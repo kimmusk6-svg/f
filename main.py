98ad097b3b0ec035d18f2908d93ad6545e2299ed
kimmusk6-svg <kimmusk6@gmail.com>
2026-02-09 14:23:31 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, "你好！我已经成功在 Railway 上活过来了！")
+    bot.reply_to(message, "说你想说的任何话")
 
 print("Bot started...")
 bot.infinity_polling()
